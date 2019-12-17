5b3acec675065043ecac6052949c1648814a221c
mishozz <zlatev.1@abv.bg>
2019-12-17 18:08:40 +0200

    Adding Alpha Beta Prunning
    Adding alpha beta prunning optimization to the minimax algorithm.

--- Tic Tac Toe/Tic Tac Toe/SinglePlayerGame.hpp
+++ Tic Tac Toe/Tic Tac Toe/SinglePlayerGame.hpp
@@ -1,478 +1,11 @@
 #include "Game.h"
 using namespace std;
 
-/*
-class SinglePlayer:public Game {
-private:	
-	int evaluate(Board board);
-	int minimax(Board board, int depth, bool isMax);
-	Position findBestMove(Board board);
-public:
-	void startGame();
-};
-
-
-int SinglePlayer::evaluate(Board board) {
-	Position pos;
-	for (int row = 0; row<3; row++)
-	{
-		if (board.getSymbolAt(row, 0) == board.getSymbolAt(row, 1) &&
-			board.getSymbolAt(row, 1) == board.getSymbolAt(row, 2))
-		{
-			if (board.getSymbolAt(row, 0) == playerTwo.getSymbol())
-				return +10;
-			else if (board.getSymbolAt(row, 0) == playerOne.getSymbol())
-				return -10;
-		}
-	}
-
-	// Checking for Columns for X or O victory. 
-	for (int col = 0; col<3; col++)
-	{
-		
-		if (board.getSymbolAt(0,col) == board.getSymbolAt(1, col) &&
-			board.getSymbolAt(1, col) == board.getSymbolAt(2, col))
-		{
-			if (board.getSymbolAt(0, col) == playerTwo.getSymbol())
-				return +10;
-
-			else if (board.getSymbolAt(0, col) == playerOne.getSymbol())
-				return -10;
-		}
-	}
-
-	// Checking for Diagonals for X or O victory. 
-	if (board.getSymbolAt(0, 0) == board.getSymbolAt(1, 1) && board.getSymbolAt(1, 1) == board.getSymbolAt(2, 2))
-	{
-		if (board.getSymbolAt(0, 0) == playerTwo.getSymbol())
-			return +10;
-		else if (board.getSymbolAt(0, 0) == playerOne.getSymbol())
-			return -10;
-	}
-
-	if (board.getSymbolAt(0, 2) == board.getSymbolAt(1, 1) && board.getSymbolAt(1, 1) == board.getSymbolAt(2, 0))
-	{
-		if (board.getSymbolAt(0, 2) == playerTwo.getSymbol())
-			return +10;
-		else if (board.getSymbolAt(0, 2) == playerOne.getSymbol())
-			return -10;
-	}
-
-	// Else if none of them have won then return 0 
-	return 0;
-}
-
-int SinglePlayer::minimax(Board board, int depth, bool isMax)
-{
-	int score = evaluate(board);
-
-	// If Maximizer has won the game return his/her 
-	// evaluated score 
-	if (score == 10)
-		return score;
-
-	// If Minimizer has won the game return his/her 
-	// evaluated score 
-	if (score == -10)
-		return score;
-
-	// If there are no more moves and no winner then 
-	// it is a tie 
-	if (board.isFull())
-		return 0;
-
-	// If this maximizer's move 
-	if (isMax)
-	{
-		int best = -1000;
-
-		// Traverse all cells 
-		for (int i = 0; i<3; i++)
-		{
-			for (int j = 0; j<3; j++)
-			{
-				Position pos(i, j);
-				// Check if cell is empty 
-				if (board.getSymbolAt(pos) == BLANK_SPACE)
-				{
-					
-					// Make the move 
-					board.setSymbolAt(pos,playerTwo.getSymbol());
-
-					// Call minimax recursively and choose 
-					// the maximum value 
-					best = max(best,
-						minimax(board, depth + 1, !isMax));
-
-					// Undo the move 
-					board.setSymbolAt(pos, BLANK_SPACE);
-				}
-			}
-		}
-		return best;
-	}
-
-	// If this minimizer's move 
-	else
-	{
-		int best = 1000;
-
-		// Traverse all cells 
-		for (int i = 0; i<3; i++)
-		{
-			for (int j = 0; j<3; j++)
-			{
-				Position pos(i, j);
-				// Check if cell is empty 
-				if (board.getSymbolAt(pos) == BLANK_SPACE)
-				{
-					// Make the move 
-					board.setSymbolAt(pos, playerOne.getSymbol());
-
-					// Call minimax recursively and choose 
-					// the minimum value 
-					best = min(best,
-						minimax(board, depth + 1, !isMax));
-
-					// Undo the move 
-					board.setSymbolAt(pos, BLANK_SPACE);
-				}
-			}
-		}
-		return best;
-	}
-}
-
-
-Position SinglePlayer::findBestMove(Board board) {
-	int bestVal = -1000;
-	Position bestMove(-1,-1);
-
-	// Traverse all cells, evaluate minimax function for 
-	// all empty cells. And return the cell with optimal 
-	// value. 
-	for (int i = 0; i<3; i++)
-	{
-		for (int j = 0; j<3; j++)
-		{
-			Position pos(i, j);
-			// Check if cell is empty 
-			if (board.getSymbolAt(pos) == BLANK_SPACE)
-			{
-				// Make the move 
-				board.setSymbolAt(pos, playerTwo.getSymbol());
-
-				// compute evaluation function for this 
-				// move. 
-				int moveVal = minimax(board, 0, false);
-
-				// Undo the move 
-				board.setSymbolAt(pos, BLANK_SPACE);
-
-				// If the value of the current move is 
-				// more than the best value, then update 
-				// best/ 
-				if (moveVal > bestVal)
-				{
-					bestMove.setX (i);
-					bestMove.setY(j);
-					bestVal = moveVal;
-				}
-			}
-		}
-	}
-
-	printf("The value of the best Move is : %d\n\n",
-		bestVal);
-
-	return bestMove;
-}
-
-
-void SinglePlayer::startGame() {
-	
-
-	turnCount = 0;
-	system("CLS");
-
-	cout << "Enter X or O: ";
-	inputSymbol();
-	system("CLS");
-	Position move;
-	while (!isGameOver() && turnCount != MAX_TURNS) {
-
-		system("CLS");
-		if (playerOne.isOnturn()) {
-			cout << board;
-			cout << "Position: ";
-			board.inputValidPositionInBoard();
-			system("CLS");
-			board.setSymbolAt(board.getPosition(),playerOne.getSymbol());
-			cout << board;
-		}
-		else {
-			move = findBestMove(board);
-			board.setSymbolAt(move, playerTwo.getSymbol());
-			cout << board;
-		}
-		turnCount++;
-		saveCurrentTurnInFile("game");
-		system("CLS");
-		outputWinnerOrDraw();
-		switchTurns();
-	}
-}
-*/
-
-/*
-class SinglePlayer: public Game {
-private:
-	
-	
-
-	int evaluate(Board board);
-	int minimax(Board board, int depth, bool isMax);
-	Position findBestMove(Board board);
-	
-public:
-	void startGame();
-};
-
-
-
-
-
-
-int SinglePlayer::evaluate(Board board) {
-	// Checking for Rows for X or O victory. 
-	for (int row = 0; row<3; row++)
-	{
-		if (board.getSymbolAt(row,0) == board.getSymbolAt(row, 1) &&
-			board.getSymbolAt(row, 1) == board.getSymbolAt(row, 2))
-		{
-			if (board.getSymbolAt(row, 0) == playerTwo.getSymbol())
-				return +10;
-			else if (board.getSymbolAt(row, 0) == playerOne.getSymbol())
-				return -10;
-		}
-	}
-
-	// Checking for Columns for X or O victory. 
-	for (int col = 0; col<3; col++)
-	{
-		if (board.getSymbolAt(0, col) == board.getSymbolAt(1, col) &&
-			board.getSymbolAt(1, col) == board.getSymbolAt(2, col))
-		{
-			if (board.getSymbolAt(0, col) == playerTwo.getSymbol())
-				return +10;
-
-			else if (board.getSymbolAt(0, col) == playerOne.getSymbol())
-				return -10;
-		}
-	}
-
-	// Checking for Diagonals for X or O victory. 
-	if (board.getSymbolAt(0, 0) == board.getSymbolAt(1, 1) && board.getSymbolAt(1, 1) == board.getSymbolAt(2, 2))
-	{
-		if (board.getSymbolAt(0, 0) == playerTwo.getSymbol())
-			return +10;
-		else if (board.getSymbolAt(0, 0) == playerOne.getSymbol())
-			return -10;
-	}
-
-	if (board.getSymbolAt(0, 2) == board.getSymbolAt(1, 1) && board.getSymbolAt(1, 1) == board.getSymbolAt(2, 0))
-	{
-		if (board.getSymbolAt(0, 2) == playerTwo.getSymbol())
-			return +10;
-		else if (board.getSymbolAt(0, 2) == playerOne.getSymbol())
-			return -10;
-	}
-
-	// Else if none of them have won then return 0 
-	return 0;
-}
-
-int SinglePlayer::minimax(Board board, int depth, bool isMax)
-{
-	int score = evaluate(board);
-
-	// If Maximizer has won the game return his/her 
-	// evaluated score 
-	if (score == 10)
-		return score;
-
-	// If Minimizer has won the game return his/her 
-	// evaluated score 
-	if (score == -10)
-		return score;
-
-	// If there are no more moves and no winner then 
-	// it is a tie 
-	if (board.isFull())
-		return 0;
-
-	// If this maximizer's move 
-	if (isMax)
-	{
-		int best = -1000;
-
-		// Traverse all cells 
-		for (int i = 0; i<3; i++)
-		{
-			for (int j = 0; j<3; j++)
-			{
-				// Check if cell is empty 
-				if (board.getSymbolAt(i, j) == BLANK_SPACE)
-				{
-					// Make the move 
-					board.setSymbolAt(i, j, playerTwo.getSymbol());
-
-					// Call minimax recursively and choose 
-					// the maximum value 
-					best = max(best,
-						minimax(board, depth + 1, !isMax));
-
-					// Undo the move 
-					board.setSymbolAt(i, j, BLANK_SPACE);
-				}
-			}
-		}
-		return best;
-	}
-
-	// If this minimizer's move 
-	else
-	{
-		int best = 1000;
-
-		// Traverse all cells 
-		for (int i = 0; i<3; i++)
-		{
-			for (int j = 0; j<3; j++)
-			{
-				// Check if cell is empty 
-				if (board.getSymbolAt(i,j) == BLANK_SPACE)
-				{
-					// Make the move 
-					board.setSymbolAt(i, j, playerOne.getSymbol());
-
-					// Call minimax recursively and choose 
-					// the minimum value 
-					best = min(best,
-						minimax(board, depth + 1, !isMax));
-
-					// Undo the move 
-					board.setSymbolAt(i, j, BLANK_SPACE);
-				}
-			}
-		}
-		return best;
-	}
-}
-
-
-Position SinglePlayer::findBestMove(Board board) {
-	int bestVal = -1000;
-	Position bestMove(-1, -1);
-
-	// Traverse all cells, evaluate minimax function for 
-	// all empty cells. And return the cell with optimal 
-	// value. 
-	for (int i = 0; i<3; i++)
-	{
-		for (int j = 0; j<3; j++)
-		{
-			// Check if cell is empty 
-			if (board.getSymbolAt(i,j) == BLANK_SPACE)
-			{
-				// Make the move 
-				board.setSymbolAt(i,j, playerTwo.getSymbol());
-
-				// compute evaluation function for this 
-				// move. 
-				int moveVal = minimax(board, 0, false);
-
-				// Undo the move 
-				board.setSymbolAt(i, j, BLANK_SPACE);
-
-				// If the value of the current move is 
-				// more than the best value, then update 
-				// best/ 
-				if (moveVal > bestVal)
-				{
-					bestMove.setX(i);
-					bestMove.setY(j);
-					bestVal = moveVal;
-				}
-			}
-		}
-	}
-
-	printf("The value of the best Move is : %d\n\n",
-		bestVal);
-
-	return bestMove;
-}
-
-
-
-
-void SinglePlayer::startGame() {
-	
-
-	//turnCount = 0;
-	system("CLS");
-
-	cout << "Enter X or O: ";
-	inputSymbol();
-	system("CLS");
-	Position move;
-	while (turnCount != MAX_TURNS) {
-
-		//board.inputValidPositionInBoard();
-		system("CLS");
-		if (playerOne.isOnturn()) {
-
-			cout << board;
-			cout << "Position: ";
-			board.inputValidPositionInBoard();
-			board.setSymbolAt(board.getPosition(), playerOne.getSymbol());
-			cout << board;
-		}
-		else {
-			move = findBestMove(board);
-			board.setSymbolAt(move.getX(),move.getY(), playerTwo.getSymbol());
-			
-			cout << board;
-		}
-		turnCount++;
-		//saveCurrentTurnInFile("game");
-		system("CLS");
-		//outputWinnerOrDraw();
-		switchTurns();
-	}
-}
-*/
-
-
-
 class SinglePlayer : public Game {
 private:
-	bool isFull(char board[3][3])
-	{
-		for (int i = 0; i < BOARD_SIZE; i++)
-		{
-			for (int j = 0; j < BOARD_SIZE; j++)
-			{
-				if (board[i][j] == BLANK_SPACE)
-					return false;
-			}
-		}
-		return true;
-	}
 	int evaluate(Board board);
-	int minimax(Board board, int depth, bool isMax);
-	Position findBestMove(Board board);
+	int minimax(Board board, int depth, bool isMax, int alpha, int beta);
+	Position findBestMove(Board board, int turnCount);
 	void decideWinner()const;
 public:
 	void startGame();
@@ -493,15 +26,16 @@ void SinglePlayer::decideWinner()const {
 
 
 int SinglePlayer::evaluate(Board board) {
+	
 	// Checking for Rows for X or O victory. 
 	for (int row = 0; row<3; row++)
 	{
-		if (board.board[row][0] == board.board[row][1] &&
-			board.board[row][1] == board.board[row][2])
+		if (board(row,0) == board(row, 1) &&
+			board(row, 1) == board(row, 2))
 		{
-			if (board.board[row][0] == playerTwo.getSymbol())
+			if (board(row, 0) == playerTwo.getSymbol())
 				return +10;
-			else if (board.board[row][0] == playerOne.getSymbol())
+			else if (board(row, 0) == playerOne.getSymbol())
 				return -10;
 		}
 	}
@@ -509,51 +43,55 @@ int SinglePlayer::evaluate(Board board) {
 	// Checking for Columns for X or O victory. 
 	for (int col = 0; col<3; col++)
 	{
-		if (board.board[0][col] == board.board[1][col] &&
-			board.board[1][col] == board.board[2][col])
+		if (board(0,col) == board(1, col) &&
+			board(1, col) == board(2, col))
 		{
-			if (board.board[0][col] == playerTwo.getSymbol())
+			if (board(0, col) == playerTwo.getSymbol())
 				return +10;
 
-			else if (board.board[0][col] == playerOne.getSymbol())
+			else if (board(0, col) == playerOne.getSymbol())
 				return -10;
 		}
 	}
 
 	// Checking for Diagonals for X or O victory. 
-	if (board.board[0][0] == board.board[1][1] && board.board[1][1] == board.board[2][2])
+	if (board(0, 0) == board(1, 1) && board(1, 1) == board(2, 2))
 	{
-		if (board.board[0][0] == playerTwo.getSymbol())
+		if (board(0, 0) == playerTwo.getSymbol())
 			return +10;
-		else if (board.board[0][0] == playerOne.getSymbol())
+		else if (board(0, 0) == playerOne.getSymbol())
 			return -10;
 	}
 
-	if (board.board[0][2] == board.board[1][1] && board.board[1][1] == board.board[2][0])
+	if (board(0, 2) == board(1, 1) && board(1, 1) == board(2, 0))
 	{
-		if (board.board[0][2] == playerTwo.getSymbol())
+		if (board(0, 2) == playerTwo.getSymbol())
 			return +10;
-		else if (board.board[0][2] == playerOne.getSymbol())
+		else if (board(0, 2) == playerOne.getSymbol())
 			return -10;
 	}
 
 	// Else if none of them have won then return 0 
 	return 0;
+	
 }
 
-int SinglePlayer::minimax(Board board, int depth, bool isMax)
+
+
+int SinglePlayer::minimax(Board board, int depth, bool isMax,int alpha ,int beta)
 {
+	
 	int score = evaluate(board);
 
 	// If Maximizer has won the game return his/her 
 	// evaluated score 
 	if (score == 10)
-		return score;
+		return score - depth;
 
 	// If Minimizer has won the game return his/her 
 	// evaluated score 
 	if (score == -10)
-		return score;
+		return score + depth;
 
 	// If there are no more moves and no winner then 
 	// it is a tie 
@@ -566,23 +104,23 @@ int SinglePlayer::minimax(Board board, int depth, bool isMax)
 		int best = -1000;
 
 		// Traverse all cells 
-		for (int i = 0; i<3; i++)
+		for (int i = 0; i < BOARD_SIZE; i++)
 		{
-			for (int j = 0; j<3; j++)
+			for (int j = 0; j < BOARD_SIZE; j++)
 			{
 				// Check if cell is empty 
-				if (board.board[i][j] == BLANK_SPACE)
+				if (board(i, j) == BLANK_SPACE)
 				{
 					// Make the move 
-					board.board[i][j] = playerTwo.getSymbol();
+					board(i, j) = playerTwo.getSymbol();
 
-					// Call minimax recursively and choose 
-					// the maximum value 
-					best = max(best,
-						minimax(board, depth + 1, !isMax));
+					int val = minimax(board, depth + 1, !isMax, alpha, beta);
+					best = max(best, val);
+					alpha = max(alpha, val);
 
-					// Undo the move 
-					board.board[i][j] = BLANK_SPACE;
+					board(i, j) = BLANK_SPACE;
+					if (beta <= alpha)
+						break;
 				}
 			}
 		}
@@ -595,23 +133,26 @@ int SinglePlayer::minimax(Board board, int depth, bool isMax)
 		int best = 1000;
 
 		// Traverse all cells 
-		for (int i = 0; i<3; i++)
+		for (int i = 0; i < BOARD_SIZE; i++)
 		{
-			for (int j = 0; j<3; j++)
+			for (int j = 0; j < BOARD_SIZE; j++)
 			{
 				// Check if cell is empty 
-				if (board.board[i][j] == ' ')
+				if (board(i, j) == BLANK_SPACE)
 				{
 					// Make the move 
-					board.board[i][j] = playerOne.getSymbol();
+					board(i, j) = playerOne.getSymbol();
 
-					// Call minimax recursively and choose 
-					// the minimum value 
-					best = min(best,
-						minimax(board, depth + 1, !isMax));
+					int val = minimax(board, depth + 1, isMax, alpha, beta);
+					best = min(best, val);
+					alpha = min(alpha, val);
+
+					board(i, j) = BLANK_SPACE;
+					if (beta <= alpha)
+						break;
 
 					// Undo the move 
-					board.board[i][j] = BLANK_SPACE;
+					board(i, j) = BLANK_SPACE;
 				}
 			}
 		}
@@ -620,10 +161,15 @@ int SinglePlayer::minimax(Board board, int depth, bool isMax)
 }
 
 
-Position SinglePlayer::findBestMove(Board board) {
+Position SinglePlayer::findBestMove(Board board,int turnCount) {
 	int bestVal = -1000;
 	Position bestMove(-1, -1);
 
+	if(board(1, 1) == BLANK_SPACE && turnCount == 1){
+		bestMove.setX(1);
+		bestMove.setY(1);
+		return bestMove;
+	}
 	// Traverse all cells, evaluate minimax function for 
 	// all empty cells. And return the cell with optimal 
 	// value. 
@@ -632,17 +178,17 @@ Position SinglePlayer::findBestMove(Board board) {
 		for (int j = 0; j<BOARD_SIZE; j++)
 		{
 			// Check if cell is empty 
-			if (board.board[i][j] == BLANK_SPACE)
+			if (board(i, j) == BLANK_SPACE)
 			{
 				// Make the move 
-				board.board[i][j] = playerTwo.getSymbol();
+				board(i, j) = playerTwo.getSymbol();
 
 				// compute evaluation function for this 
 				// move. 
-				int moveVal = minimax(board, 0, false);
+				int moveVal = minimax(board, 0, false,INT_MIN,INT_MAX);
 
 				// Undo the move 
-				board.board[i][j] = BLANK_SPACE;
+				board(i, j) = BLANK_SPACE;
 
 				// If the value of the current move is 
 				// more than the best value, then update 
@@ -657,41 +203,6 @@ Position SinglePlayer::findBestMove(Board board) {
 		}
 	}
 
-	printf("The value of the best Move is : %d\n\n",
-		bestVal);
-
 	return bestMove;
 }
 
-
-
-
-void SinglePlayer::startGame() {	
-	system("CLS");
-	cout << "Enter X or O: ";
-	inputSymbol();
-	system("CLS");
-	Position move;
-	while (!isGameOver() && turnCount != MAX_TURNS) {
-
-		//board.inputValidPositionInBoard();
-		system("CLS");
-		if (playerOne.isOnturn()) {	
-			cout << board;		
-			cout << "Position: ";
-			board.inputValidPositionInBoard();
-			board.setSymbolAt(board.getPosition(), playerOne.getSymbol());
-			cout << board;
-		}
-		else {
-			move = findBestMove(board);
-			board.board[move.getX()][move.getY()] = playerTwo.getSymbol();
-			cout << board;
-		}
-		turnCount++;
-		saveCurrentTurnInFile("game");
-		system("CLS");
-		outputWinnerOrDraw();
-		switchTurns();
-	}
-}
